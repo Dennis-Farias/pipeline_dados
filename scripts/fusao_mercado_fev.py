1d2c572350fd20dc0619314697ba14814313289f
Dennis-Farias <dennisfarias3@gmail.com>
2025-10-18 16:21:41 -0300

    Criando uma classe para o pipeline de dados

--- scripts/fusao_mercado_fev.py
+++ scripts/fusao_mercado_fev.py
@@ -1,87 +1,19 @@
-import json
-import csv
-
-def leitura_json(path_json):
-    dados_json = []
-    with open(path_json, 'r') as file:
-        dados_json = json.load(file)
-    return dados_json
-
-def leitura_csv(path_csv):
-    dados_csv = []
-    with open(path_csv, 'r') as file:
-        spamreader = csv.DictReader(file, delimiter=',')
-        for row in spamreader:
-            dados_csv.append(row)
-    return dados_csv
-
-def leitura_dados(path, tipo_arquivo):
-    dados = []
-
-    if tipo_arquivo == 'csv':
-        dados = leitura_csv(path)
-    elif tipo_arquivo == 'json':
-        dados = leitura_json(path)
-    
-    return dados
-
-def get_columns(dados):
-    return list(dados[-1].keys())
-
-def rename_columns(dados, key_mapping):
-    new_dados_csv = []
-
-    for old_dict in dados:
-        dict_temp = {}
-        for old_key, value in old_dict.items():
-            dict_temp[key_mapping[old_key]] = value 
-        new_dados_csv.append(dict_temp)
-
-    return new_dados_csv
-
-def size_data(dados):
-    return len(dados)
-
-def join(dadosA, dadosB):
-    combined_list = []
-    combined_list.extend(dadosA)
-    combined_list.extend(dadosB)
-    return combined_list
-
-def tranformando_dados_tabela(dados, nomes_colunas):
-    dados_combinados_tabela = [nomes_colunas]
-
-    for row in dados:
-        linha = []
-        for coluna in nomes_colunas:
-            linha.append(row.get(coluna, 'Indisponível'))
-        dados_combinados_tabela.append(linha)
-    
-    return dados_combinados_tabela
-
-def salvando_dados(dados, path):
-    with open(path, 'w') as file:
-        writer = csv.writer(file)
-        writer.writerows(dados)
+from processamento_dados import Dados
 
 path_json = 'data_raw/dados_empresaA.json'
 path_csv = 'data_raw/dados_empresaB.csv'
 
-# Iniciando a leitura
-dados_json = leitura_dados(path_json, 'json')
-nome_colunas_json = get_columns(dados_json)
-tamanho_dados_json = size_data(dados_json)
+# Extract
 
-print(f"Nome colunas dados json:: {nome_colunas_json}")
-print(f"Tamanho dos dados json: {tamanho_dados_json}")
+dados_empresaA = Dados(path_json, 'json')
+print(f"\nNomes das colunas da empresa A: {dados_empresaA.nome_colunas}\n")
+print(f"Quantidade de linhas da empresa A: {dados_empresaA.qtd_linhas}\n")
 
-dados_csv = leitura_dados(path_csv, 'csv')
-nome_colunas_csv = get_columns(dados_csv)
-tamanho_dados_csv = size_data(dados_csv)
-print(f"Nome colunas dados csv: {nome_colunas_csv}")
-print(f"Tamanho dos dados csv: {tamanho_dados_csv}")
+dados_empresaB = Dados(path_csv, 'csv')
+print(f"Nomes das colunas da empresa B: {dados_empresaB.nome_colunas}\n")
+print(f"Quantidade de linhas da empresa B: {dados_empresaB.qtd_linhas}\n")
 
-# Transformação dos dados
+# Transform
 
 key_mapping = {'Nome do Item': 'Nome do Produto',
                 'Classificação do Produto': 'Categoria do Produto',
@@ -90,22 +22,15 @@ key_mapping = {'Nome do Item': 'Nome do Produto',
                 'Nome da Loja': 'Filial',
                 'Data da Venda': 'Data da Venda'}
 
-dados_csv = rename_columns(dados_csv, key_mapping)
-nome_colunas_csv = get_columns(dados_csv)
-print(nome_colunas_csv)
+dados_empresaB.rename_columns(key_mapping)
+print(f"Novos nomes de colunas: {dados_empresaB.nome_colunas}\n")
 
-dados_fusao = join(dados_json, dados_csv)
-nome_colunas_fusao = get_columns(dados_fusao)
-tamanho_dados_fusao = size_data(dados_fusao)
-print(nome_colunas_fusao)
-print(tamanho_dados_fusao)
+dados_fusao = Dados.join(dados_empresaA, dados_empresaB)
+print(f"Nomes das colunas da fusão das empresas: {dados_fusao.nome_colunas}\n")
+print(f"Quantidade de linhas da fusão das empresas: {dados_fusao.qtd_linhas}\n")
 
-# Salvando dados
-
-dados_fusao_tabela = tranformando_dados_tabela(dados_fusao, nome_colunas_fusao)
+# Load
 
 path_dados_combinados = 'data_processed/dados_combinados.csv'
-
-salvando_dados(dados_fusao_tabela, path_dados_combinados)
-
-print(path_dados_combinados)
+dados_fusao.salvando_dados(path_dados_combinados)
+print(f"Caminho para os dados combinados: {path_dados_combinados}\n")
